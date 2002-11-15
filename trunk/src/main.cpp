a6becff4bdb53f8e593ce43303a7dfb890f85e47
mcneight <mcneight@33f9a114-732f-0410-b705-a1c7a9eb1d63>
2002-11-15 21:33:38 +0000

    Completely moved all GLUT routines to graphics.cpp

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -55,33 +55,18 @@ static const char rcsid[] = "";
 
 using namespace std;
 
-void display(void);
-void text(void);
-void keyboard(unsigned char key, int mouse_x, int mouse_y);
-void mouse(int button, int state, int x, int y);
-
 TLE			sat;
 vector<TLE>		sats;
 vector<TLE>::iterator	satIter;
-ECI			posECI;
-LLA			posLLA;
 
+/*
 long double A;
 long double B;
 long double E;
 long double I_o;
 long double PER;
 long double RA;
-
-float camerax = e_R * 2;
-float cameray = e_R * 2;
-float cameraz = e_R * 2;
-
-int mainWindow;
-int textWindow;
-
-bool positionWindow = true;
-bool eciOutput = true;
+*/
 
 int main(int argc, char *argv[])
 {
@@ -173,29 +158,7 @@ int main(int argc, char *argv[])
   
 #else
 
-  // Start GLUT routines
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-  glutInitWindowSize(500, 500);
-  // Create the main window
-  mainWindow = glutCreateWindow("GLsat version 0.2");
-  // Initializa OpenGL
-  init();
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshapeMain);
-  // Define keyboard function
-  glutKeyboardFunc(keyboard);
-  //glutMouseFunc(mouse);
-  glutIdleFunc(display);
-  // For the coordinates sub-window:
-  // Create the coordinates sub-window
-  textWindow = glutCreateSubWindow(mainWindow, 0, 0, 500, 100);
-  glutDisplayFunc(text);
-  glutReshapeFunc(reshapeText);
-  // Define keyboard function
-  glutKeyboardFunc(keyboard);
-  glutIdleFunc(text);
-  glutMainLoop();
+  GLgraphics(argc, argv, sats);
   
 #endif
   
@@ -204,268 +167,8 @@ int main(int argc, char *argv[])
 
 
 
-/*
- * display()
- *
- * The GLUT calls this function whenever the window must be redrawn.
- *  
- */
-void display(void)
-{
-  /*
-  long double x = 0;
-  long double y = 0;
-  long double z = 0;
-  */
-  time_t			currentTime;
-  long double			timeJD = 0;
-  
-  glutSetWindow(mainWindow);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  glLoadIdentity();
-
-  // Switch from right-hand to left-hand coordinate space
-  // glScalef(1.0, 1.0, -1.0);
-
-  // Place the camera
-  gluLookAt(camerax, cameray, cameraz, 0, 0, 0, 0, 1, 0);
 
 
-  /*
-   * These two lines cause the rendered objects to rotate about the y axis.
-   * To for faster rotation, increase rotationIncrement, and vice versa.
-   * How fast this actually rotatesdepends on the speed of the computer
-   * and graphics hardware.
-   */
-
-  // static float rotationIncrement = 1000;
-  // glRotatef(rotationIncrement, 0, 1.0, 0);
-  // glRotatef(rotationIncrement, 0, 0, 1.0);
-
-  BlueMarble();
-
-  currentTime = time(NULL);
-  timeJD = julianDate(gmtime(&currentTime));
-  
-  for (satIter = sats.begin(); satIter != sats.end(); satIter++)
-  {
-    posECI = satIter->position4(timeJD);
-  
-#ifdef DEBUG
-    cout << asctime(gmtime(&currentTime));
-    cout << posECI << endl;
-    posLLA = posECI;
-    cout << posLLA << endl;
-#endif
-
-  // x represents semi-major axis
-  // y represents semi-minor axis
-  // z represents orbital inclination
-  // 
-  // x = 
-  // y = A_0 * sqrt(1 - e_0 * e_0)
-  //   =  beta_0
-  // z = inclination * sin(i)
-  /*
-  glBegin(GL_LINE_LOOP);
-    glColor3f(1,0,0);
-    // go from 0 to 2 \Pi in \frac{\Pi}{4} steps
-    for (i = 0; i < (2 * PI); i += M_PI_4)
-    {
-      glVertex3f( (cos(i + RA) * (A * e_R)),
-                  (sin(i + PER) * (B * e_R)),
-                  (I_o * sin(i)) * e_R
-                );
-    }
-  glEnd();
-  */
-  
-    // Put a big red square pixel where the satellite should be
-    /*
-    glPointSize(100);
-    glBegin(GL_POINTS);
-      glColor3f(1,0,0);
-      glVertex3d(posECI.X, posECI.Y, posECI.Z);
-    glEnd();
-    */
-    
-    // Put a teapot where the satellite should be
-    glPushMatrix();
-      glColor3d(0.0, 1.0, 0.0);
-      glTranslated(posECI.X, posECI.Y, posECI.Z);
-      glutWireTeapot(100);
-    glPopMatrix();
-
-    // Slap a label next to the teapot
-    glRasterPos3d(posECI.X,posECI.Y,posECI.Z);
-    printString(posECI.name);
-
-    // draw a line from the satellite to the center of the earth
-    glBegin(GL_LINES);
-      glColor3d(1.0, 1.0, 1.0);
-      glVertex3d(posECI.X,posECI.Y,posECI.Z);
-      glVertex3d(0.0, 0.0, 0.0);
-    glEnd();
-
-  }
-
-  glutSwapBuffers();
-  glFlush();
-        
-  glutPostRedisplay();
-
-  // sleep(1);
-}
 
 
-void text(void)
-{
-  glutSetWindow(textWindow);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  
-  glColor3f(0.9, 0.9, 0.9);
-  glRasterPos2i(0, 13);
-  
-  if (eciOutput)
-  {
-    // Name
-    glRasterPos2i(0, 13);
-    printString(posECI.name);
-    // X
-    glRasterPos2i(0, 26);
-    printString(posECI.stringX());
-    // Y
-    glRasterPos2i(0, 39);
-    printString(posECI.stringY());
-    // Z
-    glRasterPos2i(0, 52);
-    printString(posECI.stringZ());
-  }
-  else
-  {
-    posLLA = posECI;
-    // Name
-    glRasterPos2i(0, 13);
-    printString(posLLA.name);
-    // Lat
-    glRasterPos2i(0, 26);
-    printString(posLLA.stringLat());
-    // Y
-    glRasterPos2i(0, 39);
-    printString(posLLA.stringLon());
-    // Z
-    glRasterPos2i(0, 52);
-    printString(posLLA.stringAlt());
-  }
-  
-  // Testing stuff...
-  
-  glutSwapBuffers();
-  glFlush();
-  
-  glutPostRedisplay();
-}
-
-
-/*
- * keyboard()
- *       Gets called when a button on the keyboard is pressed.
- *       x is a char containing the ascii value of the pressed button.
- *       mouse_x and mouse_y are the coordinates of the mouse in the
- *       window at time of keystroke.
- */
-void keyboard(unsigned char key, int mouse_x, int mouse_y)
-{
-  switch (key)
-  {
-    case 'd':
-    case 'D':
-      posLLA.switchOutput();
-      break;
-    case 'l':
-    case 'L':
-      eciOutput = !eciOutput;
-      break;
-    case 'm':
-    case 'M':
-      break;
-    case 'o':
-    case 'O':
-      camerax = posECI.X;
-      cameray = posECI.Y;
-      cameraz = posECI.Z;
-      break;
-    case 'p':
-    case 'P':
-      if (positionWindow)
-      {
-	glutSetWindow(textWindow);
-	glutHideWindow();
-      }
-      else
-      {
-	glutSetWindow(textWindow);
-	glutShowWindow();
-      }
-      positionWindow = !positionWindow;
-      break;
-    case 'q':
-    case 'Q':
-    case 27:
-      exit(0);
-      break;
-    case 's':
-    case 'S':
-      camerax *= 1.3;
-      cameray *= 1.3;
-      cameraz *= 1.3;
-      break;
-    case 'w':
-    case 'W':
-      camerax /= 1.3;
-      cameray /= 1.3;
-      cameraz /= 1.3;
-      break;
-    case 'x':
-    case 'X':
-      camerax = 10000;
-      cameray = 0;
-      cameraz = 0;
-      break;
-    case 'y':
-    case 'Y':
-      camerax = 1;
-      cameray = 10000;
-      cameraz = 1;
-      break;
-    case 'z':
-    case 'Z':
-      camerax = 0;
-      cameray = 0;
-      cameraz = 10000;
-      break;
-  }
-
-  glutPostRedisplay();
-}
-
-
-/*
- *
- */
-void mouse(int button, int state, int x, int y)
-{
-  if ((button == GLUT_LEFT_BUTTON) && (state==GLUT_DOWN))
-  {
-    camerax += x;
-    cameray += y;
-  }
-
-  if(button == GLUT_LEFT_BUTTON && state==GLUT_UP)
-  {
-    
-  }
-  glutPostRedisplay();
-}
 
