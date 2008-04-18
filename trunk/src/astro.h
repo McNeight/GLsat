8b5ac1ddffaaeb2a3db087393568851d842a2b9b
mcneight <mcneight@33f9a114-732f-0410-b705-a1c7a9eb1d63>
2008-04-18 08:34:15 +0000

    (first commit only applied to one file) * Merged code from /trunk/GLsat-Cocoa/src to /trunk/src * Deleted /trunk/GLsat-Cocoa/src

--- trunk/src/astro.h
+++ trunk/src/astro.h
@@ -32,5 +32,7 @@ long double julianDate(const struct tm *date);
 long double julianDateFromNow();
 long double ThetaG_JD(long double jd_L);
 ECI sunPosition(const long double jd);
+ECI sunPositionLow(const long double jd);
+long double range360(long double x);
 
 #endif
